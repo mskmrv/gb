cb38767c95d470738bc7a121a4b15979ccfb4ba2
Maksim <mx123@yandex.ru>
2019-02-09 12:43:44 +1000

    Добавил методы join() для потоков.

--- src/ru/geekbrains/classes/lesson5/MultyThreading.java
+++ src/ru/geekbrains/classes/lesson5/MultyThreading.java
@@ -1,5 +1,7 @@
 package ru.geekbrains.classes.lesson5;
 
+import java.util.Arrays;
+
 public class MultyThreading {
     static final int SIZE = 10000000;
     static final int h = SIZE / 2;
@@ -11,9 +13,8 @@ public class MultyThreading {
 
     static void processArray() {
         float[] arr = new float[SIZE];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = 1.0f;
-        }
+        Arrays.fill(arr, 1.0f);
+
         long a = System.currentTimeMillis();
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
@@ -24,9 +25,8 @@ public class MultyThreading {
 
     static void treadsProcessArray() {
         float[] arr = new float[SIZE];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = 1.0f;
-        }
+        Arrays.fill(arr, 1.0f);
+
         long a = System.currentTimeMillis();
         float ar1[] = new float[h];
         System.arraycopy(arr, 0, ar1, 0, h);
@@ -36,8 +36,22 @@ public class MultyThreading {
         ArrayHandler arrayHandler1 = new ArrayHandler(ar1);
         ArrayHandler arrayHandler2 = new ArrayHandler(ar2);
 
-        new Thread(arrayHandler1).start();
-        new Thread(arrayHandler2).start();
+        Thread thread1 = new Thread(arrayHandler1);
+        thread1.start();
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Thread thread2 = new Thread(arrayHandler2);
+        thread2.start();
+        try {
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         ar1 = arrayHandler1.getArr();
         ar2 = arrayHandler1.getArr();
 
